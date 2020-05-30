0c7c6c04ca6d60532bd6e41ea7bc00aeeda11822
Niroj Thapa <nirozthapa556@gmail.com>
2020-05-30 17:15:39 +1000

    custom footer added

---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-info">
+                    <h3>Visa Assist</h3>
+                    <p>We are one of the reputed migration agent with over 10 years of experience in migration and consultation in Australia.<br><br>Remember us for your dream in Australia</p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="about.html">About us</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Services</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h4>Contact Us</h4>
+                    <p>
+                        St George Street<br>
+                        Sydney, SYD 2000<br>
+                        Australia <br>
+                        <strong>Phone:</strong> 1234567<br>
+                        <strong>Email:</strong> info@example.com<br>
+                    </p>
+
+                    <div class="social-links">
+                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-newsletter">
+                    <h4>Our Newsletter</h4>
+                    <p></p>
+                    <form action="forms/contact.php" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe" onclick="">
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+        </div>
+
+
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright. All Rights Reserved
+            </div>
+            <div class="credits">
+                Designed by Team 181
+            </div>
+        </div>
+</footer><!-- End Footer -->
